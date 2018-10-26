74a007e2925726ad23b6a72f12ecbabdfaac0b96
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2018-10-26 20:45:08 -0300

    Adiciona entidade do impulso

---
+++ src/Model/Entity/Impulse.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Model\Entity;
+
+/**
+ * @todo Document class Impulse.
+ *
+ * @author Thiago Hofmeister <thiago.souza@moovin.com.br>
+ */
+class Impulse extends EntityAbstract
+{
+    /** @var \DateTime */
+    private $created;
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        /**
+         * @todo Implement method toArray.
+         */
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function fromArray(array $array)
+    {
+        /**
+         * @todo Implement method fromArray.
+         */
+    }
+}
