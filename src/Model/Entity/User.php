28f59a5046bf74df6ccc7a006fd2027720ff8d79
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2018-10-26 19:38:50 -0300

    Adiciona propriedades essenciais para o usuário

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -12,17 +12,61 @@ use App\Model\Element;
  */
 class User extends EntityAbstract
 {
+    /** @var string */
+    private $name;
+
     /** @var string Email. */
     private $email;
 
     /** @var string */
+    private $phone;
+
+    /** @var string */
+    private $mobile;
+
+    /** @var string */
     private $password;
+
+    /** @var string CPF. */
+    private $documentNumber;
+
+    /** @var \DateTime */
+    private $dob;
+
+    /** @var string RG. */
+    private $personalDocument;
+
+    /** @var Element\User\Standard Usuário que fez o convite para o site. */
+    private $invitedBy;
 
     /** @var Element\User\Authentication Autenticação.  */
     private $authentication;
 
     /** @var \DateTime Data de cadastro. */
     private $date;
+
+    /**
+     * Retorna a propriedade {@see User::$name}.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Define a propriedade {@see User::$name}.
+     *
+     * @param string $name
+     *
+     * @return static|User
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+        return $this;
+    }
 
     /**
      * Retorna a propriedade {@see User::$email}.
@@ -44,58 +88,52 @@ class User extends EntityAbstract
     public function setEmail(string $email)
     {
         $this->email = $email;
-
         return $this;
     }
 
     /**
-     * Retorna a propriedade {@see User::$authentication}.
+     * Retorna a propriedade {@see User::$phone}.
      *
-     * @return Element\User\Authentication
+     * @return string
      */
-    public function getAuthentication(): ?Element\User\Authentication
+    public function getPhone(): string
     {
-        return $this->authentication;
+        return $this->phone;
     }
 
     /**
-     * Define a propriedade {@see User::$authentication}.
+     * Define a propriedade {@see User::$phone}.
      *
-     * @param Element\User\Authentication $authentication
+     * @param string $phone
      *
      * @return static|User
      */
-    public function setAuthentication(?Element\User\Authentication $authentication)
+    public function setPhone(string $phone)
     {
-        $this->authentication = $authentication;
-
+        $this->phone = $phone;
         return $this;
     }
 
     /**
-     * Retorna a propriedade {@see User::$date}.
+     * Retorna a propriedade {@see User::$mobile}.
      *
-     * @return \DateTime
+     * @return string
      */
-    public function getDate(): \DateTime
+    public function getMobile(): string
     {
-        if (empty($this->date)) {
-            $this->date = new \DateTime();
-        }
-        return $this->date;
+        return $this->mobile;
     }
 
     /**
-     * Define a propriedade {@see User::$date}.
+     * Define a propriedade {@see User::$mobile}.
      *
-     * @param \DateTime $date
+     * @param string $mobile
      *
      * @return static|User
      */
-    public function setDate(\DateTime $date)
+    public function setMobile(string $mobile)
     {
-        $this->date = $date;
-
+        $this->mobile = $mobile;
         return $this;
     }
 
@@ -123,15 +161,160 @@ class User extends EntityAbstract
     }
 
     /**
+     * Retorna a propriedade {@see User::$documentNumber}.
+     *
+     * @return string
+     */
+    public function getDocumentNumber(): string
+    {
+        return $this->documentNumber;
+    }
+
+    /**
+     * Define a propriedade {@see User::$documentNumber}.
+     *
+     * @param string $documentNumber
+     *
+     * @return static|User
+     */
+    public function setDocumentNumber(string $documentNumber)
+    {
+        $this->documentNumber = $documentNumber;
+        return $this;
+    }
+
+    /**
+     * Retorna a propriedade {@see User::$dob}.
+     *
+     * @return \DateTime
+     */
+    public function getDob(): \DateTime
+    {
+        return $this->dob;
+    }
+
+    /**
+     * Define a propriedade {@see User::$dob}.
+     *
+     * @param \DateTime $dob
+     *
+     * @return static|User
+     */
+    public function setDob(\DateTime $dob)
+    {
+        $this->dob = $dob;
+        return $this;
+    }
+
+    /**
+     * Retorna a propriedade {@see User::$personalDocument}.
+     *
+     * @return string
+     */
+    public function getPersonalDocument(): string
+    {
+        return $this->personalDocument;
+    }
+
+    /**
+     * Define a propriedade {@see User::$personalDocument}.
+     *
+     * @param string $personalDocument
+     *
+     * @return static|User
+     */
+    public function setPersonalDocument(string $personalDocument)
+    {
+        $this->personalDocument = $personalDocument;
+        return $this;
+    }
+
+    /**
+     * Retorna a propriedade {@see User::$invitedBy}.
+     *
+     * @return Element\User\Standard
+     */
+    public function getInvitedBy(): Element\User\Standard
+    {
+        return $this->invitedBy;
+    }
+
+    /**
+     * Define a propriedade {@see User::$invitedBy}.
+     *
+     * @param Element\User\Standard $invitedBy
+     *
+     * @return static|User
+     */
+    public function setInvitedBy(Element\User\Standard $invitedBy)
+    {
+        $this->invitedBy = $invitedBy;
+        return $this;
+    }
+
+    /**
+     * Retorna a propriedade {@see User::$authentication}.
+     *
+     * @return Element\User\Authentication
+     */
+    public function getAuthentication(): Element\User\Authentication
+    {
+        return $this->authentication;
+    }
+
+    /**
+     * Define a propriedade {@see User::$authentication}.
+     *
+     * @param Element\User\Authentication $authentication
+     *
+     * @return static|User
+     */
+    public function setAuthentication(Element\User\Authentication $authentication)
+    {
+        $this->authentication = $authentication;
+        return $this;
+    }
+
+    /**
+     * Retorna a propriedade {@see User::$date}.
+     *
+     * @return \DateTime
+     */
+    public function getDate(): \DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * Define a propriedade {@see User::$date}.
+     *
+     * @param \DateTime $date
+     *
+     * @return static|User
+     */
+    public function setDate(\DateTime $date)
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
      * @inheritDoc
      */
     public function toArray(): array
     {
         $toArray = [
+            'name' => $this->getName(),
             'email' => $this->getEmail(),
+            'phone' => $this->getPhone(),
+            'mobile' => $this->getMobile(),
+            'password' => $this->getPassword(),
+            'documentNumber' => $this->getDocumentNumber(),
+            'dob' => $this->getDob(),
+            'personalDocument' => $this->getPersonalDocument(),
+            'invitedBy' => !empty($this->getInvitedBy()) ? $this->getInvitedBy()->toArray() : null,
             'authentication' => !empty($this->getAuthentication()) ? $this->getAuthentication()->toArray() : null,
             'date' => $this->getDate()->format(Date::JAVASCRIPT_ISO_FORMAT),
-            'password' => $this->getPassword()
         ];
 
         if (!empty($this->getId())) {
@@ -147,9 +330,16 @@ class User extends EntityAbstract
     public static function fromArray(array $array)
     {
         return (new static($array['_id']))
+            ->setName($array['name'])
             ->setEmail($array['email'])
+            ->setPhone($array['phone'])
+            ->setMobile($array['mobile'])
+            ->setPassword($array['password'])
+            ->setDocumentNumber($array['documentNumber'])
+            ->setDob($array['dob'])
+            ->setPersonalDocument($array['personalDocument'])
+            ->setInvitedBy(!empty((array) $array['invitedBy']) ? Element\User\Standard::fromArray((array) $array['invitedBy']) : null)
             ->setAuthentication(!empty((array) $array['authentication']) ? Element\User\Authentication::fromArray((array) $array['authentication']) : null)
-            ->setDate(new \DateTime($array['date']))
-            ->setPassword($array['password']);
+            ->setDate(new \DateTime($array['date']));
     }
 }
