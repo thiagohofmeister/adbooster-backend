9a2faecc6f93e763f73a2c603afaad5b00847f8b
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2018-10-26 20:38:14 -0300

    Adiciona enum de status do anuncio

---
+++ src/Enum/Announcement/Status.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Enum\Announcement;
+
+use Eloquent\Enumeration\AbstractEnumeration;
+
+/**
+ * Enum de status do anúncio.
+ *
+ * @method static Status ACTIVE()
+ * @method static Status INACTIVE()
+ * @method static Status PAUSED()
+ *
+ * @author Thiago Hofmeister <thiago.souza@moovin.com.br>
+ */
+class Status extends AbstractEnumeration
+{
+    /** @var string */
+    const ACTIVE = 'active';
+
+    /** @var string */
+    const INACTIVE = 'inactive';
+
+    /** @var string */
+    const PAUSED = 'paused';
+}
